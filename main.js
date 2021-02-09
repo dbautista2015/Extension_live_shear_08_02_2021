617bced07e46ecc18bb141989e15a7b9eccd8993
Diana Patricia Bautista Otalora <diana.bautista.otalora@gmail.com>
2021-02-08 21:15:17 -0500

    clase tiempo real

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+let cadena = "Hola como estas";
+
+//console.log(cadena.endsWith("estas"));  //Busca al final de la cadena si existe la palabra y devuelve un booleano como respuesta (true/false)
+//console.log(cadena.startsWith("Hola")); //Busca al inicio de la cadena si existe la palabra y devuelve un booleano como respuesta (true/false)
+//console.log(cadena.lastIndexOf("o"));  //Busca dentro de toda la cadena la coincidencia en la palabra de derecha a izquierda y devuelve la primera coincidencia con su posición
+//console.log(cadena.indexOf("como")); //Busca dentro de toda la cadena la coincidencia en la palabra de izquierda a derecha y nos devuelve la primera coincidencia con su posición
+//console.log(cadena.includes("como")); //Busca dentro de toda la cadena si existe la palabra y devuelve un booleano como respuesta (true/false)
+
+//Modificando la cadena
+//let resultado = cadena.repeat(4); //Repite la cadena la cantidad de veces que ingresamos 
+
+//Modificamos pero nos devuelve diferentes tipos de datos (array, number, object)
+//let resultado = cadena.split(" ", 4); // Nos devuelve un array dependiendo del separador y el limite de resultados que deseemos para el ejemplo son los primeros 4
+//let resultado = cadena.substring(0,12); //se captura una parte de la cadena por medio de las posiciones de cada uno de los caracteres
+//let resultado = cadena.toUpperCase(); // transforma la cadena de texto a mayúsculas
+//let resultado = cadena.toLowerCase(); // transforma la cadena de texto a minúscula
+//console.log(resultado)
+
